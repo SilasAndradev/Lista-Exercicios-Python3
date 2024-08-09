f729ee41d7a75bd29cc94735756b4388d6c58d43
Silas Andrade <silasandrade.code@gmail.com>
2024-08-09 18:05:41 -0300

    Create desafio10.guanabara.py
    Criei um programa que lê quanto dinheiro uma pessoa tem na carteira e mostra quantos dólares ela pode comprar.

---
+++ desafio10.guanabara.py
@@ -0,0 +1,5 @@
+#Desafio 10 - Curso em Vídeo
+#Crie um programa que lê quanto dinheiro uma pessoa tem na carteira e mostra quantos dólares ela pode comprar. 
+n = float(input('Quanto você tem na carteira? '))
+d = n / 3.27
+print('Você pode compra {} dólares'.format(d))
