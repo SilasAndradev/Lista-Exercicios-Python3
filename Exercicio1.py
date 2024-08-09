2c6b12595cb5b36bedabd428368dea1cfd571ccd
Silas Andrade <silasandrade.code@gmail.com>
2024-08-09 16:54:01 -0300

    Create Exercicio1.py

---
+++ Exercicio1.py
@@ -0,0 +1,6 @@
+#Código que verifica se o caractere digitado pelo usuário é numérico ou não
+code = input('Digite algo: ')
+if code.isnumeric():
+  print('É um número')
+else:
+    print('Não é uma número')
