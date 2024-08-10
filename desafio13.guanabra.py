a2aa8b1e43c06ab1ca3cbb49f3f090f7f477961e
Silas Andrade <silasandrade.code@gmail.com>
2024-08-10 10:40:46 -0300

    Create desafio13.guanabra.py
    Fiz um algoritmo que lê o salário de um funcionário e mostra seu novo salário, com 15% de aumento.

---
+++ desafio13.guanabra.py
@@ -0,0 +1,5 @@
+#Desafio 13 - Curso em Vídeo
+#Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+salario = float(input('Digite o preço do produto: '))
+salarioAumento = salario + salario * 0.15
+print('O salário antigo era de {}, com o novo aumento, o salário ira ser de: {}.'.format(salario, salarioAumento))
