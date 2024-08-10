036e28336f79a608220dbe9f230a6ab5ad1c8d99
Silas Andrade <silasandrade.code@gmail.com>
2024-08-10 10:59:20 -0300

    Update desafio6.guanabara.py
    Fiz um programa que lê um número e mostre o seu dobro, triplo e raiz quadrada.

--- desafio6.guanabara.py
+++ desafio6.guanabara.py
@@ -1,8 +1,7 @@
 #Desafio 6 - Curso em Vídeo
 #Faça um programa que leia um número e mostre o seu dobro, triplo e raiz quadrada.
-import math
 n = int(input('Digite um número: '))
-r = (math.sqrt(n))
+r = n ** (1/2)
 d = n * 2
 t = n * 3
 print('A triplo de {} é {}\nO seu dobro é {}\nE a raiz quadrada é {}'.format(n, t, d, r))
