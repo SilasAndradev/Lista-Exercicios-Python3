32bb6d59016b23c757cb83992fceacf5fe238f34
Silas Andrade <silasandrade.code@gmail.com>
2024-08-10 16:47:40 -0300

    Create desafio16.guanabara.py

---
+++ desafio16.guanabara.py
@@ -0,0 +1,5 @@
+#Desafio 16 - Curso em Vídeo
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira 
+import math
+num = float(input('Digite um número: '))
+print(math.floor(num))
