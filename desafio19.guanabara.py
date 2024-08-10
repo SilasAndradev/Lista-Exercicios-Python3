a3919bce6e3861b16cd5ee8df2a7ada946920daf
Silas Andrade <silasandrade.code@gmail.com>
2024-08-10 17:48:49 -0300

    Update desafio19.guanabara.py

--- desafio19.guanabara.py
+++ desafio19.guanabara.py
@@ -1,5 +1,9 @@
 #Desafio 19 - Curso em Vídeo
 #Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
 import random
-names = ["Silas", "Yulo", "Oséias", "Pedro"]
+a1 = input('Digite o nome: ')
+a2 = input('Digite o nome: ')
+a3 = input('Digite o nome: ')
+a4 = input('Digite o nome: ')
+names = [a1, a2, a3, a4]
 print("A pessoa que irá apagar o quadro será:",format(random.choice(names)))
