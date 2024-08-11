d9e1f5926ef03b5c60d169e29becc93b67c0c2db
Silas Andrade <silasandrade.code@gmail.com>
2024-08-11 17:21:15 -0300

    Delete desafio23.guanabra.py

--- desafio23.guanabra.py
+++
@@ -1,7 +0,0 @@
-#Desaio 23 - Curso em Vídeo
-#Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
-num = input('Digite um número de 0 a 9999: ')
-print(("""unidade: {}
-dezena: {}
-centena: {}
-milhar: {}""".format(num[0], num[1], num[2], num[3])))
