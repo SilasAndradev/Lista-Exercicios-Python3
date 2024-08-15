39d1477457ae779c11e2f47b5bf65b0708ca9f16
Silas Andrade <silasandrade.code@gmail.com>
2024-08-15 13:45:28 -0300

    Update desafio28.guanabra.py

--- desafio28.guanabra.py
+++ desafio28.guanabra.py
@@ -2,8 +2,11 @@
 #Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o núemro escolhido pelo computador.
 #O programa deverá escrever na tela se o usuário venceu ou perdeu.
 import random
+from time import sleep
 num = random.randint(0, 5)
 num2 = int(input('Digite um número de 0 a 5: '))
+print('PROCESSANDO...')
+sleep(5)
 if num2 == num:
   print('Você acertou! Eu pensei no número {}.'.format(num))
 else:
