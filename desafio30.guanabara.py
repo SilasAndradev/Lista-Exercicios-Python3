da52bfa3a6f917422f83353d54b134eaf4afb614
Silas Andrade <silasandrade.code@gmail.com>
2024-08-16 16:48:03 -0300

    Update desafio30.guanabara.py

--- desafio30.guanabara.py
+++ desafio30.guanabara.py
@@ -1,1 +1,6 @@
-https://replit.com/@silasandradecod/myproject1
+#Desafio 3o - Curso em Vídeo
+num = int(input("Digite um número: "))
+if num & 2 == 0:
+    print("O número que você digitou é PAR")
+else:
+    print("O número que você digitou é IMPAR")
