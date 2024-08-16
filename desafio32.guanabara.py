fb97510667535855c5386d556a88020953f799cc
Silas Andrade <silasandrade.code@gmail.com>
2024-08-16 17:03:25 -0300

    Update desafio32.guanabara.py

--- desafio32.guanabara.py
+++ desafio32.guanabara.py
@@ -1,11 +1,7 @@
-#Desafio 32 - Curso em Vídeo
+#Desafio 31 - Curso em Vídeo
 #Faça um programa que leia um ano qualquer e mostre se ela é Bissexto.
 ano = int(input('Digite o ano: '))
-if ano % 4 == 0 and ano % 100 != 0:
-    print('Na primeira situação: O ano é Bissexto!')
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano é Bissexto!')
 else:
-    print('Na primeira situação: O ano não é Bissexto!')
-if ano % 4 != 0 and ano % 400 != 0:
-  print('Na segunda situação: O ano não é Bissexto!')
-else:
-    print('Na segunda situação: O ano é Bissexto!')
+    print('O ano não é Bissexto!')
