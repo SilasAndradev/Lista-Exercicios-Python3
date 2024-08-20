d52d87c093de4ad71629df94ed7664f08193fd73
Silas Andrade <silasandrade.code@gmail.com>
2024-08-20 18:44:49 -0300

    Update desafio42.guanabara.py

--- desafio42.guanabara.py
+++ desafio42.guanabara.py
@@ -1,8 +1,8 @@
-#Desafio 42  - Curso em Vídeo
-#Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado
-#- Equilátero: todos os lados iguais
-#- Isósceles: dois lados iguais
-#- Escaleno: todos os lados diferente
+  #Desafio 42  - Curso em Vídeo
+  #Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado
+  #- Equilátero: todos os lados iguais
+  #- Isósceles: dois lados iguais
+  #- Escaleno: todos os lados diferente
 ladoA = float(input('Digite o lado A do triângulo: '))
 ladoB = float(input('Digite o lado B do triângulo: '))
 ladoC = float(input('Digite o lado C do triângulo: '))
@@ -12,7 +12,7 @@ if ladoA < ladoB + ladoC and ladoB < ladoA + ladoB and ladoC < ladoA + ladoB:
       print('O triângulo formado é um Equilátero.')
     elif ladoA != ladoB != ladoC != ladoA:
       print('O triângulo formado é um Escaleno.')
-    elif ladoB in (ladoC, ladoA):
+    else:
       print('O triângulo formado será um Isósceles.')
 else:
     print('As três retas NÃO PODEM formar um triângulo.')
