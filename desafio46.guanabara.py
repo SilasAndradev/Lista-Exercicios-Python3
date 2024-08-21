5828b59602f87ad1fb287d7edcbaa3d0a51c2e47
Silas Andrade <silasandrade.code@gmail.com>
2024-08-21 15:01:55 -0300

    Update desafio46.guanabara.py

--- desafio46.guanabara.py
+++ desafio46.guanabara.py
@@ -1,1 +1,8 @@
-
+#Desafio 46  - Curso em Vídeo
+#Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício de 10 a 0, com uma pausa de 1 segundo entre eles.
+import time
+print('Os fogos irão estourar em:')
+for c in range(10, -1, -1):
+  print(c)
+  time.sleep(1)
+print('Kabuuuum!!!')
