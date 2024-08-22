7e491a035968a0a632cc3a7d96886f23e11f90ec
Silas Andrade <silasandrade.code@gmail.com>
2024-08-22 20:36:15 -0300

    Create desafio48.guanabara.py

---
+++ desafio48.guanabara.py
@@ -0,0 +1,8 @@
+#Desafio 48  - Curso em Vídeo
+#Faça um programa que calcule a soma entre todos os números impares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+soma = 0
+for c in range(1, 500):
+  if c % 3 == 0 and c % 2 != 0:
+    soma = soma + c
+    print(c)
+print(soma)
