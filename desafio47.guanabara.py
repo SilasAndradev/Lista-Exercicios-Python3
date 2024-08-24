4ff33ae185ad87330adc3687171b028382e61399
Silas Andrade <silasandrade.code@gmail.com>
2024-08-24 13:56:59 -0300

    Update desafio47.guanabara.py

--- desafio47.guanabara.py
+++ desafio47.guanabara.py
@@ -1,4 +1,5 @@
 #Desafio 47  - Curso em Vídeo
 #Crie um programa que mostra na tela todos os números pares que estão no intervalo entre 1 e 50
 for c in range(1+1, 50+1, 2):
-  print(c)
+  print(c, end=' ')
+print('Fim :)')
