a467918bc9d0f32a3be00577283cdf5d5a403b5f
Silas Andrade <silasandrade.code@gmail.com>
2024-08-28 18:40:16 -0300

    Update desafio50.guanabara.py

--- desafio50.guanabara.py
+++ desafio50.guanabara.py
@@ -1,8 +1,10 @@
 #Desafio 50  - Curso em Vídeo
 #Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que foram pares. Se o valor digitado for ímpar, desconsidere-o
 soma = 0
+con = 0
 for c in range(1, 6):
   num = int(input('Digite um número: '))
   if num % 2 == 0:
+    con += 1
     soma = soma + num
-print('A soma de todos os números pares são',soma)
+print('Você informou {} números Pares, e soma deles é {}.'.format(con, soma))
