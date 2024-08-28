7848474a66211517ff26c120365bd70db839b018
Silas Andrade <silasandrade.code@gmail.com>
2024-08-28 20:34:58 -0300

    Update desafio54.guanabara.py

--- desafio54.guanabara.py
+++ desafio54.guanabara.py
@@ -1,11 +1,13 @@
 #Desafio 54  - Curso em Vídeo
 #Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+from datetime import date
 acum = 0
 acum1 = 0
+atual = date.today().year
 for c in range(1, 8):
   num = int(input('Digite o ano do seu nascimento: '))
-  if 2024 - num >= 18:
-    acum = acum + 1
-  if 2024 - num < 18:
-    acum1 = acum1 + 1
+  if (atual - num) >= 25:
+    acum += 1
+  else:
+    acum1 += 1
 print('{} pessoas já são de maior e {} ainda não são de maior.'.format(acum, acum1))
