23bc58b6f3a0cc1181a63a1dee55753db8fa3e15
Silas <silasandrade.code@gmail.com>
2024-09-07 11:53:37 -0300

    Create desafio76REVISAR.py

---
+++ desafio76REVISAR.py
@@ -0,0 +1,11 @@
+#Desafio 76 - Curso em Video
+a = int(input('Digite um número: '))
+b = int(input('Digite um número: '))
+c = int(input('Digite um número: '))
+d = int(input('Digite um número: '))
+c = 0
+minha_tupla = (a, b ,c, d)
+print(f'Você digitou os valores {minha_tupla}')
+print(f'O valor 9 apareceu {minha_tupla.count(9)} vezes.')
+print(f'O valor 3 apareceu na {minha_tupla.index(3) + 1} posição.')
+#REVISAR
