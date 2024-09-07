a96bfb5facbee1ac590339847fe1cc8536eb691c
Silas <silasandrade.code@gmail.com>
2024-09-07 11:34:17 -0300

    Update desafio74.guanabara.py

--- desafio74.guanabara.py
+++ desafio74.guanabara.py
@@ -1,4 +1,6 @@
 #Desafio 74 - Curso em Video
 import random
 minha_tupla = tuple(random.sample(range(10), 5))
-print(sorted(minha_tupla))
+print(minha_tupla)
+tupla = sorted(minha_tupla)
+print(f'O menor número é: {tupla[0]}\nO maior número é: {tupla[4]}')
