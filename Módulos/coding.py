9b0d59ace3e1704cad47ca62be66ad83e5ec3a9e
Silas <silasandrade.code@gmail.com>
2024-10-19 17:45:05 +0000

    main

--- Módulos/coding.py
+++ Módulos/coding.py
@@ -1,20 +1,10 @@
-def fatorial(n):
-    f = 1 
-    for c in range(1,n+1):
-        f *= c
-    return f
-
-def dobro(n):
-    return n * 2
-
-def triplo(n):
-    return n * 3
+from uteis import números
 
 num = int(input("Digite um valor: "))
 
-fat = uteis.fatorial(num)
-triplo = uteis.triplo(num)
-dobro = uteis.dobro(num)
+fat = números.fatorial(num)
+triplo = números.triplo(num)
+dobro = números.dobro(num)
 
 print(f'O fatorial de {num} é {fat} :)')
 print(f'O fatorial de {num} é {triplo}')
