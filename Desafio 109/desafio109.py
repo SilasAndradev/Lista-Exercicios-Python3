9c0a85cb91b602163699f232c68a1392f7d8f927
Silas <silasandrade.code@gmail.com>
2024-10-19 20:00:51 +0000

    main

--- Desafio 109/desafio109.py
+++ Desafio 109/desafio109.py
@@ -1,8 +1,4 @@
 import moeda
 
 preço = float(input('Digite o preço: '))
-
-print(f'O preço {moeda.moeda(preço)} aumentando 10% é igual: {(moeda.aumentar(preço, True))}')
-print(f'O preço {moeda.moeda(preço)} diminuindo 10% é igual: {(moeda.diminuir(preço, True))}')
-print(f'O preço {moeda.moeda(preço)} pela metade é igual: {(moeda.metade(preço, True))}')
-print(f'O preço {moeda.moeda(preço)} dobrando é igual: {(moeda.dobro(preço, True))}')
+moeda.resumo(preço, 10, 13)
--- Desafio 109/moeda.py
+++ Desafio 109/moeda.py
@@ -1,18 +1,29 @@
-def aumentar(n, format = False):
-    res = n + (n * (10/100)) 
-    return res if format == False else moeda(res)
+def aumentar(n= 0, taxa = 0, format = False):
+    res = n + (n * (taxa/100)) 
+    return res if format is False else moeda(res)
 
-def diminuir(n, format = False):
-    res = n - (n * (10/100))
-    return res if format == False else moeda(res)
+def diminuir(n = 0, taxa = 0, format = False):
+    res = n - (n * (taxa/100))
+    return res if format is False else moeda(res)
 
-def dobro(n, format = False):
+def dobro(n = 0, format = False):
     res = n * 2 
-    return res if format == False else moeda(res)
+    return res if not format else moeda(res)
 
-def metade(n, format = False):
+def metade(n = 0, format = False):
     res = n / 2
-    return res if format == False else moeda(res)
+    return res if not format else moeda(res)
 
 def moeda(n):
-    return f'R${n:.2f}'.replace('.',',')
+    return f'R${n:>.2f}'.replace('.',',')
+
+def resumo(n, aumento, redução):
+    print('-'*30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-'*30)
+    print(f'Preço Analisado: \t{moeda(n)}')
+    print(f'Dobro do Preço: \t{dobro(n, True)}')
+    print(f'Metade do Preço: \t{metade(n, True)}')
+    print(f'{aumento}% de aumento: \t{aumentar(n, aumento, True)}')
+    print(f'{redução}% de redução: \t{diminuir(n, redução, True)}')
+    print('-'*30)
