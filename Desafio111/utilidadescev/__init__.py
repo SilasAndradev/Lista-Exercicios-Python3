2a7462090131435c4d20e8894a091bebf297a336
Silas <silasandrade.code@gmail.com>
2024-10-20 15:52:38 +0000

    MIAN

--- Desafio111/utilidadescev/__init__.py
+++
@@ -1,1 +0,0 @@
-from Desafio111.utilidadescev import moeda, dados
--- ex111/desafio111.py
+++ ex111/desafio111.py
@@ -1,4 +1,4 @@
-from Desafio111.utilidadescev import moeda
+from utilidadescev import moeda
 
 preço = float(input('Digite o preço: '))
 moeda.resumo(preço, 10, 13)
